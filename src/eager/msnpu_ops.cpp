67315d4612ca71994347915413bae3d2712b7668
Ashwin Hari <ashari@microsoft.com>
2021-08-01 17:14:23 -0700

    Custom extension compiles.

--- src/eager/msnpu_ops.cpp
+++ src/eager/msnpu_ops.cpp
@@ -103,37 +103,39 @@ std::vector<at::Tensor> transformerdecoder(
 }
 
 std::vector<at::Tensor>  transformerdecodergrad(
-    int64_t padded_hidden_size, int64_t head_size, int64_t num_heads, torch::Tensor norm_1_weight,
-                                     torch::Tensor q_weight,
-                                     Tensor k_weight,
-                                     Tensor v_weight,
-                                     Tensor attention_mask,
-                                     Tensor dense_weight,
-                                     Tensor mlp_weight_0,
-                                     Tensor mlp_weight_1,
-                                     Tensor norm_2_weight,
-                                     Tensor pad_values,
-                                     Tensor grad_input,
-                                     Tensor norm_1_dmem,
-                                     Tensor norm_1_std_inv_dmem,
-                                     Tensor norm_1_shift_dmem,
-                                     Tensor q_dmem,
-                                     Tensor k_dmem,
-                                     Tensor v_dmem,
-                                     Tensor softmax_dmem,
-                                     Tensor soft_dropout_dmem,
-                                     Tensor soft_dropout_mask_dmem,
-                                     Tensor v_soft_dmem,
-                                     Tensor dense_dropout_mask_dmem,
-                                     Tensor norm_2_dmem,
-                                     Tensor norm_2_std_inv_dmem,
-                                     Tensor norm_2_shift_dmem,
-                                     Tensor mlp_hidden_1_dmem,
-                                     Tensor mlp_gelu_dmem,
-                                     Tensor mlp_dropout_mask_dmem)
+    int64_t padded_hidden_size, int64_t head_size, int64_t num_heads, const torch::Tensor& norm_1_weight,
+                                     const torch::Tensor& q_weight,
+                                     const torch::Tensor& k_weight,
+                                     const torch::Tensor& v_weight,
+                                     const torch::Tensor& attention_mask,
+                                     const torch::Tensor& dense_weight,
+                                     const torch::Tensor& mlp_weight_0,
+                                     const torch::Tensor& mlp_weight_1,
+                                     const torch::Tensor& norm_2_weight,
+                                     const torch::Tensor& pad_values,
+                                     const torch::Tensor& grad_input,
+                                     const torch::Tensor& norm_1_dmem,
+                                     const torch::Tensor& norm_1_std_inv_dmem,
+                                     const torch::Tensor& norm_1_shift_dmem,
+                                     const torch::Tensor& q_dmem,
+                                     const torch::Tensor& k_dmem,
+                                     const torch::Tensor& v_dmem,
+                                     const torch::Tensor& softmax_dmem,
+                                     const torch::Tensor& soft_dropout_dmem,
+                                     const torch::Tensor& soft_dropout_mask_dmem,
+                                     const torch::Tensor& v_soft_dmem,
+                                     const torch::Tensor& dense_dropout_mask_dmem,
+                                     const torch::Tensor& norm_2_dmem,
+                                     const torch::Tensor& norm_2_std_inv_dmem,
+                                     const torch::Tensor& norm_2_shift_dmem,
+                                     const torch::Tensor& mlp_hidden_1_dmem,
+                                     const torch::Tensor& mlp_gelu_dmem,
+                                     const torch::Tensor& mlp_dropout_mask_dmem)
 {
-  auto& invoker = GetORTInvoker(embeddings_post_dropout.device());
+  auto& invoker = GetORTInvoker(norm_1_weight.device());
   const  std::string ort_op_name = "TransformerDecoderGrad";
+  constexpr size_t num_outputs = 17;
+  constexpr size_t num_attrs = 3;
 
   // Create ORT attributes
   onnxruntime::NodeAttributes attrs(num_attrs);
@@ -176,18 +178,19 @@ std::vector<at::Tensor>  transformerdecodergrad(
   auto ort_in_mlp_dropout_mask_dmem = create_ort_value(invoker, mlp_dropout_mask_dmem);
 
   // Create ORTValues for output tensors.
-  constexpr size_t num_outputs = std::tuple_size<transformer_decoder_grad_out_list>;
   std::vector<OrtValue> ort_outputs(num_outputs);
 
   // Invoke the transformer decoder command on the ORT device
   auto status = invoker.Invoke(
       ort_op_name,
-      {ort_in_embeddings_post_dropout, ort_in_normalization_1_w,
-       ort_in_normalization_1_b, ort_in_query_w, ort_in_query_b, ort_in_key_w,
-       ort_in_key_b, ort_in_value_w, ort_in_value_b, ort_in_attention_mask,
-       ort_in_project_w, ort_in_project_b, ort_in_FFN1_w, ort_in_FFN1_b,
-       ort_in_FFN2_w, ort_in_FFN2_b, ort_in_normalization_2_w,
-       ort_in_normalization_2_b, ort_in_pad_values},
+      {ort_in_norm_1_weight, ort_in_q_weight,
+       ort_in_k_weight, ort_in_v_weight, ort_in_attention_mask, ort_in_dense_weight,
+       ort_in_mlp_weight_0, ort_in_mlp_weight_1, ort_in_norm_2_weight, ort_in_pad_values,
+       ort_in_grad_input, ort_in_norm_1_dmem, ort_in_norm_1_std_inv_dmem, ort_in_norm_1_shift_dmem,
+       ort_in_q_dmem, ort_in_k_dmem, ort_in_v_dmem,
+       ort_in_softmax_dmem, ort_in_soft_dropout_dmem, ort_in_soft_dropout_mask_dmem, ort_in_v_soft_dmem, ort_in_dense_dropout_mask_dmem,
+       ort_in_norm_2_dmem, ort_in_norm_2_std_inv_dmem, ort_in_norm_2_shift_dmem, ort_in_mlp_hidden_1_dmem, ort_in_mlp_gelu_dmem,
+       ort_in_mlp_dropout_mask_dmem},
       ort_outputs, &attrs, onnxruntime::kMSDomain);
 
   if (!status.IsOK()) {
@@ -199,7 +202,7 @@ std::vector<at::Tensor>  transformerdecodergrad(
   std::vector<at::Tensor> outputs(num_outputs);
   for (size_t i = 0; i < num_outputs; i++) {
     outputs[i] = aten_tensor_from_ort(std::move(ort_outputs[i]),
-                                      embeddings_post_dropout.options());
+                                      norm_1_weight.options());
   }
 
   return outputs;
--- src/eager/msnpu_ops.h
+++ src/eager/msnpu_ops.h
@@ -6,7 +6,7 @@ namespace eager {
 namespace msnpu {
 
 static const char * const TransformerDecoderName = "transformerdecoder";
-static const char * const TransformerDecoderName = "transformerdecodergrad";
+static const char * const TransformerDecoderGradName = "transformerdecodergrad";
 
 std::vector<at::Tensor> transformerdecoder(
     int64_t padded_hidden_size, int64_t head_size, float soft_dropout_prob,
@@ -26,34 +26,34 @@ std::vector<at::Tensor> transformerdecoder(
     const torch::Tensor& normalization_2_b, const torch::Tensor &pad_values);
 
 std::vector<at::Tensor> transformerdecodergrad(
-    int64_t padded_hidden_size, int64_t head_size, int64_t num_heads, torch::Tensor norm_1_weight,
-                                     torch::Tensor q_weight,
-                                     Tensor k_weight,
-                                     Tensor v_weight,
-                                     Tensor attention_mask,
-                                     Tensor dense_weight,
-                                     Tensor mlp_weight_0,
-                                     Tensor mlp_weight_1,
-                                     Tensor norm_2_weight,
-                                     Tensor pad_values,
-                                     Tensor grad_input,
-                                     Tensor norm_1_dmem,
-                                     Tensor norm_1_std_inv_dmem,
-                                     Tensor norm_1_shift_dmem,
-                                     Tensor q_dmem,
-                                     Tensor k_dmem,
-                                     Tensor v_dmem,
-                                     Tensor softmax_dmem,
-                                     Tensor soft_dropout_dmem,
-                                     Tensor soft_dropout_mask_dmem,
-                                     Tensor v_soft_dmem,
-                                     Tensor dense_dropout_mask_dmem,
-                                     Tensor norm_2_dmem,
-                                     Tensor norm_2_std_inv_dmem,
-                                     Tensor norm_2_shift_dmem,
-                                     Tensor mlp_hidden_1_dmem,
-                                     Tensor mlp_gelu_dmem,
-                                     Tensor mlp_dropout_mask_dmem);
+    int64_t padded_hidden_size, int64_t head_size, int64_t num_heads, const torch::Tensor& norm_1_weight,
+                                     const torch::Tensor& q_weight,
+                                     const torch::Tensor& k_weight,
+                                     const torch::Tensor& v_weight,
+                                     const torch::Tensor& attention_mask,
+                                     const torch::Tensor& dense_weight,
+                                     const torch::Tensor& mlp_weight_0,
+                                     const torch::Tensor& mlp_weight_1,
+                                     const torch::Tensor& norm_2_weight,
+                                     const torch::Tensor& pad_values,
+                                     const torch::Tensor& grad_input,
+                                     const torch::Tensor& norm_1_dmem,
+                                     const torch::Tensor& norm_1_std_inv_dmem,
+                                     const torch::Tensor& norm_1_shift_dmem,
+                                     const torch::Tensor& q_dmem,
+                                     const torch::Tensor& k_dmem,
+                                     const torch::Tensor& v_dmem,
+                                     const torch::Tensor& softmax_dmem,
+                                     const torch::Tensor& soft_dropout_dmem,
+                                     const torch::Tensor& soft_dropout_mask_dmem,
+                                     const torch::Tensor& v_soft_dmem,
+                                     const torch::Tensor& dense_dropout_mask_dmem,
+                                     const torch::Tensor& norm_2_dmem,
+                                     const torch::Tensor& norm_2_std_inv_dmem,
+                                     const torch::Tensor& norm_2_shift_dmem,
+                                     const torch::Tensor& mlp_hidden_1_dmem,
+                                     const torch::Tensor& mlp_gelu_dmem,
+                                     const torch::Tensor& mlp_dropout_mask_dmem);
 }
 } // namespace eager
 } // namespace torch_ort
